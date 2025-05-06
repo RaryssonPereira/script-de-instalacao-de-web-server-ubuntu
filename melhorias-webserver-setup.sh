e75aed990714f3014e7c0f9d14225c1592e6dca5
Rarysson Pereira <145616416+RaryssonPereira@users.noreply.github.com>
2025-05-06 14:59:23 -0300

    Update melhorias-webserver-setup.sh

--- melhorias-webserver-setup.sh
+++ melhorias-webserver-setup.sh
@@ -1,1 +1,10 @@
+#! /bin/bash
 
+# Caminho onde será salvo o log desse script
+LOG_FILE="/var/log/setup_base.log"
+
+# Verifica se o script já foi executado antes
+if [ -f "$LOG_FILE=" ]; then
+    echo "Não vamos seguir o script webserver-setup. Já foi executado antes em: $(cat $LOG_FILE). Saindo..."
+    exit
+fi
