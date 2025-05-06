a8b8aaf050e869149fd1b765ace37ced619138e1
Rarysson Pereira <145616416+RaryssonPereira@users.noreply.github.com>
2025-05-06 17:39:08 -0300

    Update melhorias-webserver-setup.sh

--- melhorias-webserver-setup.sh
+++ melhorias-webserver-setup.sh
@@ -11,6 +11,17 @@ fi
 
 # Define o Hostname do Servidor
 configure_hostname(){
-    # Consulta o IP dele
-    IP_SERVER=$(curl -s https://api.ipify.org) # 
+    # Consulta o IP público do Servidor usando um serviço externo
+    IP_SERVER=$(curl -s https://api.ipify.org) # O -s no curl significa "silent". Ele suprime a barra de progresso e mensagens de erro que normalmente são exibidas no terminal.
+    # Faz a consulta reversa do IP para obter o Hostname
+    REVERSE_HOSTNAME=$(host "$IP_SERVER" | awk '/pointer/ {print $5}' | sed 's/\.$//')
+
+    echo "Hostname atual: $(hostname)"
+    echo "Hostname reverso detectado: $REVERSE_HOSTNAME"
+    read -p "Digite o novo hostname (ou N para não alterar): " NEW_HOSTNAME
+
+    if [[ "$NEW_HOSTNAME" != "N" && "$NEW_HOSTNAME" != "n" ]]; then
+        hostnamectl set-hostname "$NEW_HOSTNAME"
+        echo "127.0.1.1 $NEW_HOSTNAME" >> /etc/hosts
+    fi
 }
