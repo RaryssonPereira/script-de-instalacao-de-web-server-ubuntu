6f6879bda161427ea6a26b93ce2bb3abe2898d1b
Rarysson Pereira <145616416+RaryssonPereira@users.noreply.github.com>
2025-05-06 17:23:20 -0300

    Update melhorias-webserver-setup.sh

--- melhorias-webserver-setup.sh
+++ melhorias-webserver-setup.sh
@@ -3,8 +3,14 @@
 # Caminho onde será salvo o log desse script
 LOG_FILE="/var/log/setup_base.log"
 
-# Verifica se o script já foi executado antes
+# Verifica se esse Script (webserver-setup.sh) já foi executado antes
 if [ -f "$LOG_FILE=" ]; then
     echo "Não vamos seguir o script webserver-setup. Já foi executado antes em: $(cat $LOG_FILE). Saindo..."
     exit
 fi
+
+# Define o Hostname do Servidor
+configure_hostname(){
+    # Consulta o IP dele
+    IP_SERVER=$(curl -s https://api.ipify.org) # 
+}
